5306c423ca813a2f153b57a026247760d7e34483
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2026-02-17 12:08:41 +0100

    add SqlSyntaxExtensions for automated tests only

---
+++ src/Umbraco.Infrastructure/Persistence/SqlSyntaxExtensions.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+using System.Reflection;
+using NPoco;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Infrastructure.Persistence.SqlSyntax;
+
+namespace Umbraco.Extensions;
+
+/// <summary>
+///     Provides extension methods to <see cref="ISqlSyntaxProvider" />.
+/// </summary>
+public static class SqlSyntaxExtensions
+{
+    /// <summary>
+    ///     Gets a quoted table and field name.
+    /// </summary>
+    /// <typeparam name="TDto">The type of the DTO.</typeparam>
+    /// <param name="sqlSyntax">An <see cref="ISqlSyntaxProvider" />.</param>
+    /// <param name="fieldSelector">An expression specifying the field.</param>
+    /// <param name="tableAlias">An optional table alias.</param>
+    /// <returns></returns>
+    public static string GetFieldName<TDto>(this ISqlSyntaxProvider sqlSyntax, Expression<Func<TDto, object?>> fieldSelector, string? tableAlias = null)
+        => sqlSyntax.GetFieldName(fieldSelector, tableAlias);
+}
